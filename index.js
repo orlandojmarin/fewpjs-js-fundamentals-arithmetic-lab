b17f8ba9ca1c377e3b9d13c8043a29744d3f62ea
Orlando Marin <orlandojermainemarin@gmail.com>
2021-02-23 10:32:13 -0800

    completed assignment

--- index.js
+++ index.js
@@ -1,2 +1,21 @@
 
 // Write your code here
+
+// multiply section
+const num1 = 31;
+const num2 = 2;
+
+const multiply = num1 * num2;
+
+//mod section
+
+const num3 = 22
+const num4 = 6
+
+const mod = num3 % num4
+
+//max section
+const max = Math.max(5, 10, 15, 20)
+
+//random section
+const random = Math.floor(Math.random()* 10 + 1);
